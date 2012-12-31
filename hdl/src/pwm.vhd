f4b22135e03996b0f50d90e35e4adbce8549b0f2
lgbeno <lgbeno@gmail.com>
2012-12-30 22:22:50 -0600

    new v2 pcb data and some hdl enhancements

--- hdl/src/pwm.vhd
+++ hdl/src/pwm.vhd
@@ -3,14 +3,11 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
 
 entity pwm is 
-generic(
-  cnt_width: integer
-);
 port(  
   clk:       in  std_logic;
   rst:       in  std_logic;
-  freq:      in  std_logic_vector(cnt_width downto 0);
-  duty:      in  std_logic_vector(cnt_width downto 0);
+  freq:      in  std_logic_vector(15 downto 0);
+  duty:      in  std_logic_vector(15 downto 0);
   dead_time: in  std_logic_vector(3 downto 0);
   pwm_out:   out std_logic;
   pwm_out_n: out std_logic
@@ -18,20 +15,20 @@ port(
 end pwm;
 
 architecture rtl of pwm is         
-  signal cnt:   std_logic_vector(cnt_width downto 0);
-  signal cmp0:  std_logic_vector(cnt_width downto 0);
-  signal cmp1:  std_logic_vector(cnt_width downto 0);
-  signal cmp2:  std_logic_vector(cnt_width downto 0);
+  signal cnt:    std_logic_vector(15 downto 0);
+  signal cmp0:   std_logic_vector(15 downto 0);
+  signal cmp1:   std_logic_vector(15 downto 0);
+  signal cmp2:   std_logic_vector(15 downto 0);
   signal cnt_en: std_logic;
-  signal slope: std_logic;
+  signal slope:  std_logic;
   begin
     process(clk, rst)
     begin
       if (rst = '1') then
         cnt       <= x"0001";
-        cmp0      <= x"0000";
-        cmp1      <= x"0000";
-        cmp2      <= x"0000";
+        cmp0      <= x"03e8";
+        cmp1      <= x"00ff";
+        cmp2      <= x"00ff";
         cnt_en    <= '1';
         slope     <= '1';
         pwm_out   <= '0';
